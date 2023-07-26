f9540e8e6daea37e29b8adfa8ff83d7de11251d1
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-07-27 01:42:48 +0300

    task.2 commit

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,4 +1,4 @@
-word = "School"
-print("First 3 letters: {}".format(word[:3]))
-print("Last 2 letters: {}".format(word[4:]))
-print("Middle word: {}".format(word[1:5]))
+school = "Holberton"
+print("First 3 letters: {}".format(school[:3]))
+print("Last 2 letters: {}".format(school[7:]))
+print("Middle word: {}".format(school[1:8]))
