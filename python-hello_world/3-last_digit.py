b0df23b49e9e655c6feb5d23254b53ec8cc9f29f
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-07-27 15:30:18 +0300

    commit task.3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -15,5 +15,7 @@ elif(last_digit==0):
 elif(last_digit<6 and last_digit!=0):
     if(number <0):
      print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number,neg_digit))
+    else:
+     print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number,last_digit))
 else:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number,last_digit))
+   print("TypeError")
