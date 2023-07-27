20bf3c36828b7b993f0560852bd04eb839e69ee4
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-07-28 00:17:37 +0300

    task 5 commit

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,6 @@
 for i in range(100):
-    if i < 10:
-        print ("0"),
-    print ("{:d}".format(i)),
+    print('{:02d}'.format(i)),
     if i < 99:
-        print (","),
+        print(","),
+    
+   
