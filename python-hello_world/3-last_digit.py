5a960837a685c09e336c03f608fedfb430f18b97
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-07-27 15:03:29 +0300

    task.3 commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,13 +6,13 @@ last_digit_str = num_str[-1]
 last_digit = int(last_digit_str)
 if(last_digit>5):
     if(number <0):
-     print("Last digit of {:d} is -{:d} and is is less than 6 and not 0\n".format(number,last_digit))
+     print("Last digit of {:d} is -{:d} and is less than 6 and not 0".format(number,last_digit))
 else:
-    print("Last digit of {:d} is {:d} and is greater than 5\n".format(number,last_digit))
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number,last_digit))
 if(last_digit==0):
-    print("Last digit of {:d} is {:d} and is 0\n".format(number,last_digit))
+    print("Last digit of {:d} is {:d} and is 0".format(number,last_digit))
 elif(last_digit<6 and last_digit!=0):
     if(number <0):
-     print("Last digit of {:d} is -{:d} and is is less than 6 and not 0\n".format(number,last_digit))
+     print("Last digit of {:d} is -{:d} and is less than 6 and not 0".format(number,last_digit))
 else:
-    print("Last digit of {:d} is {:d} and is is less than 6 and not 0\n".format(number,last_digit))
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number,last_digit))
