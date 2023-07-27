370ff051faffd6ad3a5682fcb73079cffcc25795
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-07-28 00:41:31 +0300

    commit task.6

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,1 @@
-for i in range(10):
-    for j in range(i+1, 10):
-        print("{:02d}, ".format(i), end="")
-        print("{:02d}, ".format(j), end="")
-print("89")
+for i in range(10):exec("".join((f'print("{i:02d}",end=""),'*10)).split(")")[0])
