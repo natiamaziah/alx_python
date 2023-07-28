0031bf6728977cd489d3a79fb230300c6d4df25c
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-07-28 21:08:59 +0300

    commit task.6

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,4 @@
-for i in range(10):
-    for j in range(i+1, 10):
-        print("{:02d}, ".format(i) + "{:02d}, ".format(j), end="")
-print("89")
+for i in range(8):
+  for j in range(i+1, 10):
+    print('{}{}'.format(i,j), end=", ")
+print(89)
