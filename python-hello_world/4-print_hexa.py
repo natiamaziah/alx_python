668efc280589c21c3b2dc8b15f540aa30156e568
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-07-28 14:24:19 +0300

    commit task

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,4 +1,4 @@
 def new_func():
     for x in range(99):
-      print("{:d} = {}".format(x,hex(x) ))
+      print("{:d} = {}".format(x,hex(x)))
 new_func()
--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,5 +1,3 @@
 for i in range(100):
-    print('{:02d}'.format(i))
-   
-    
+ print(f"{i:02d}", end=", " if i != 99 else "\n")
    
