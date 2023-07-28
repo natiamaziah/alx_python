2a6d56c81aa3d7bff94fbc8be5c6baa8446bdc40
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-07-28 14:16:31 +0300

    task.3  commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,18 +4,18 @@ import random
 number = random.randint(-10000, 10000)
 num_str = repr(number)
 last_digit = abs(number) % 10
-neg_num=-last_digit
-if last_digit > 5:
-   if number<0:
-      print("Last digit of" ,number, "is" ,neg_num, "and is less than 6 and not 0")
-   else:   
-    print("Last digit of", number, "is", last_digit, "and is greater than 5")
-elif last_digit == 0:
-    print("Last digit of", number, "is", last_digit, "and is 0")
-elif 0<last_digit<6:
-   if number<0:
-     print("Last digit of", number, "is", neg_num, "and is less than 6 and not 0")
-   else:    
-    print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
+neg_digit=-last_digit
+if(last_digit>5):
+    if(number<0):
+       print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number,neg_digit)) 
+    else:  
+       print("Last digit of {:d} is {:d} and is greater than 5".format(number,last_digit))
+elif(last_digit==0):
+    print("Last digit of {:d} is {:d} and is 0".format(number,last_digit))
+elif(0<last_digit<6):
+    if(number <0):
+     print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number,neg_digit))
+    else:
+     print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number,last_digit))
 
 print()
