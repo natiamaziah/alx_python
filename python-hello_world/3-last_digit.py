7709d2a5943bb4c0bc52e947c055325405ff4de6
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-07-28 14:14:15 +0300

    task3. commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -9,13 +9,13 @@ if last_digit > 5:
    if number<0:
       print("Last digit of" ,number, "is" ,neg_num, "and is less than 6 and not 0")
    else:   
-    print("The string Last digit of", number, "is", last_digit, "and is greater than 5")
+    print("Last digit of", number, "is", last_digit, "and is greater than 5")
 elif last_digit == 0:
-    print("The string Last digit of", number, "is", last_digit, "and is 0")
+    print("Last digit of", number, "is", last_digit, "and is 0")
 elif 0<last_digit<6:
    if number<0:
      print("Last digit of", number, "is", neg_num, "and is less than 6 and not 0")
    else:    
-    print("The string Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
+    print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
 
 print()
