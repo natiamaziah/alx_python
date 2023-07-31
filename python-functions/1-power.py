7421f675a63c10ce9b12606e5523607f36799ccb
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-07-31 22:10:45 +0300

    task 1.commit

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-a=int(input(""))
-b=int(input(""))  
-def pow(a,b):
-    sq=a**b
-    return(sq)
-print(pow(a,b))
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+
+def power(a, b):
+    sq = a ** b
+    return sq
+
+print(power(a, b))
