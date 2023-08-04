3f5107f0f7ad35b148cf364c3f499258133027af
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-08-04 17:34:47 +0300

    task 4 commit

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -3,7 +3,6 @@ def fibonacci_sequence(n): # return Fibonacci series up to n
     result = []
     a, b = 0, 1
     while a < n:
-        result.append(a)    # see below
+        result=result + [a]   # see below
         a, b = b, a+b
     return result
-print(fibonacci_sequence(5000))
