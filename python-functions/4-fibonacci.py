e6af26537adc36b036c30fe8e01941b2c35b46af
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-08-04 18:13:14 +0300

    task 4 commit

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,8 +1,9 @@
-def fibonacci_sequence(n): # return Fibonacci series up to n
-    """Return a list containing the Fibonacci series up to n."""
-    result = []* n
-    a, b = 0, 1
-    while a < n:
-        result=result + [a]   # see below
-        a, b = b, a+b
+def fibonacci_sequence(n):
+    result = [0, 1]
+    while len(result) < n:
+        result.append(result[-1] + result[-2])
     return result
+
+
+
+
