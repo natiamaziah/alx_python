c7053c0a6a4933c4652c1ad02ddd33e40507222f
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-08-05 20:14:29 +0300

    task 5 .commit

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,11 +1,11 @@
-
 def is_prime(number):
-    
-    if number <= 0:
+    # Ensuring that the number is greater than 1
+    if number <= 1:
         return False
-      
-    for i in range(2, int(number**0.5) + 1):
+    
+    # Checking for factors from 2 up to the number itself (excluding the number)
+    for i in range(2, number):
         if number % i == 0:
             return False
+    
     return True
-  
