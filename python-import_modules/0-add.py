b101ae04c0d05a995b6ebe2d7f50f279775f55f6
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-08-08 19:18:52 +0300

    commit task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,3 +1,4 @@
 from add_0 import add
- 
-print("{:d}+{:d}={:d}".format(1,2,1+2))
+a=1
+b=2
+print("{:d}+{:d}={:d}".format(a,b,add(a,b)))
--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,7 +1,5 @@
 def add(a,b):
-  a=1
-  b=2
   return a+b
 if __name__ == "__main__":
     result = add(1, 2)
-    print(result)
+    print("{:d}+{:d}={:d}".format(1,2,result))
