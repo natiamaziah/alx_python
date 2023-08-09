4f4d95911fca9a4e4f91e8f8cf08bf2d83717fab
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-08-09 23:58:09 +0300

    task 3. commit

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,10 +1,10 @@
 def safe_print_division(a, b):
   result= a/b
   try:
-    return result
-  except:
+    safe_print_division(a, b)
+  except ZeroDivisionError as result:
    
-    print("{:d} / {:d} = {}".format(a, b, result))
+    print("{:d} / {:d} = {}".format(a, b, None))
     return None
   finally:
     print("{:d} / {:d} = {}".format(a, b, result))
