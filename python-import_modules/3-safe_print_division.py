bb7c6f0e455e774a210b6e2c5cd17eff993720a6
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-08-09 23:51:10 +0300

    task 3. commit

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,2 +1,11 @@
 def safe_print_division(a, b):
- return a/b
+  result= a/b
+  try:
+    return result
+  except:
+   
+    print("{:d} / {:d} = {}".format(a, b, result))
+    return None
+  finally:
+    print("{:d} / {:d} = {}".format(a, b, result))
+    
