ce304d8eecd1d60dde2d8e4140ad3bef0a459bf9
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-08-13 16:20:11 +0300

    task 3

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -7,13 +7,11 @@ def safe_print_division(a, b):
         print("Inside result: {}".format(None))
         print("{:d} / {:d} = {}".format(a, b,None))
         return None
-    except TypeError:
-        print("Both inputs must be integers")
-        return None
+   
     else:
         print("Inside result: {}".format(result))
         print("{:d} / {:d} = {}".format(a, b, result))
-        return result
+        
     finally:
           
       pass
