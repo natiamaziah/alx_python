704899f6a852daf8cfb644bdee4759dcdc43a17c
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-08-13 16:07:31 +0300

    task 5.

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,7 +1,3 @@
 def raise_exception_msg(message=""):
-    try:
-      ans=raise_exception_msg("C is fun")
-      print(ans)
-    except NameError as e:
-      print(e)
+    raise NameError()
       
