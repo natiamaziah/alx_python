00b6182e1ce995350ea45a044c95f902f65032f8
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-08-15 18:23:51 +0300

    task 3..

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -4,16 +4,14 @@ def safe_print_division(a, b):
         result = int(a) / int(b)
         if b == 0:
             raise ZeroDivisionError("Division by zero not allowed.")
-        else:
-               print("Inside result: {}".format(result))
-               print("{:d} / {:d} = {}".format(a, b, result))
+        
     except ZeroDivisionError as e:
         result = None
+       
+    finally:
         print("Inside result: {}".format(result))
         print("{:d} / {:d} = {}".format(a, b, result))
-    finally:
-          return result
 
-       
+
     
      
