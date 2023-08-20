e672a14306253d480f5dc35f0e1a39304470d582
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-08-20 17:43:16 +0300

    task 1

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            print("{:2d}".format(num), end=" ")
+        for elem in row:
+            print("{:d}".format(elem), end=" ")
         print()
