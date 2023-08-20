df5244089e6a33fc9b240c5bd9d5a3f2e9f97d25
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-08-20 16:52:14 +0300

    task 1.

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,5 @@
 def print_matrix_integer(matrix=[[]]):
-   matrix = [[1, 2, 3],[4, 5, 6],[7, 8, 9 ]]
+   matrix = [[1,2,3],[4,5,6],[7,8,9]]
    for row in matrix:
         for elem in row:
             print("{:d}".format(elem),end=" ")
