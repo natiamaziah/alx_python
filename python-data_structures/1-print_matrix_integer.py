94d117902639d0ba2ca235699581c373e5514251
natiamaziah <101817217+natiamaziah@users.noreply.github.com>
2023-08-22 17:20:21 +0300

    task 1

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,4 +1,4 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        formatted_row = ' '.join(["{:>3}".format(cell) for cell in row])
+        formatted_row = ' '.join(["{:3d}".format(cell) for cell in row])
         print(formatted_row)
